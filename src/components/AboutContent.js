6c3b7f710023ebff999066706c5920b1cbc43f7b
Arihant Jain <arihantjaingit02@gmail.com>
2022-08-01 15:08:43 +0530

    class name changed

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -5,14 +5,11 @@ import resume from "../assets/Arihant-Resume.pdf"
 const AboutContent = () => {
   return (
     <div className="about">
-        <div className="left">
+        <div className="intro">
         <h1>Who am I?</h1>
         <p>
             I am a Web developer Looking for new oppurtunities to learn and grow my skills
         </p>
-        <Link to="/contact">
-            <button className="btn">contact</button>
-        </Link>
         <a href={resume} download className="btn">Download CV</a>
         </div>
     </div>
